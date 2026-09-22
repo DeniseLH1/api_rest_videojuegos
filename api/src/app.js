1327908adff89e6ec0b0939dd00b07cb84cf4b19
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 12:18:46 -0600

    fix: correxión de errores en app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import{juegoRouter}from'./routes/juego.routes.js';
+import{juegoRouter} from './routes/juego.routes.js';
 import {manejarError,rutaNoEncontrada} from './middlewares/error.middlewares.js';
 
 const API_VERSION = "1.0.0"
@@ -15,13 +15,15 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.get("health",(req,res)=>{
-    res.status(200).json({status:"ok",version:API_VERSION,servicio: "api-videojuegos"})
+app.get("/health",(req,res)=>{
+    res.status(200).json({status:"ok", version:API_VERSION,servicio: "api-videojuegos"})
 });
 
 app.get("/api/v1/preferencia",(req,res)=>{
-    res.json ({ultimaPlataforma: req.cookies.ultimaPlataforma?? null});
+    res.json ({ultimaPlataforma: req.cookies.ultimaPlataforma ?? null});
 });
+
+app.use("/api/v1/juegos", juegoRouter); 
 
 app.use(rutaNoEncontrada);
 app.use(manejarError);
