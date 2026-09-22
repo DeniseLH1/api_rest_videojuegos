c543307f831f3826025f93a8af938655601dff8c
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 14:00:19 -0600

    refactor: actualizar rutas de inicio de servidor en la raiz

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT= process.env.PORT || 3000;
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/api_juegos_clase";
+
+try{
+    await mongoose.connect(MONGO_URI);
+
+    console.log("Conectado a Mongo:",MONGO_URI);
+    
+}catch(error){
+    console.error("Error al conectar ",error.message);
+    process.exit(1);
+}
+
+app.listen(PORT,()=>{
+    console.log("Servidor Gamer listo en http//localhost:${PORT}")
+})
