9846b46439d5d5b0f11a6ef85d49ee117f9cd897
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 17:15:40 -0600

    fix: correxion de line ade codigo en el servidor

--- src/services/juego.service.js
+++ src/services/juego.service.js
@@ -3,6 +3,6 @@ export const juegoService={
     async crear(datos){return Juego.create(datos);},
     async listar(filtro={}){return Juego.find(filtro).sort({createAdt: -1})},
     async buscarPorId(id){return Juego.findById(id);},
-    async actualizar(id,datos){return Juego.frindByIdAndUpdate(id,datos,{new:true,runValidators:true});},
+    async actualizar(id,datos){return Juego.findByIdAndUpdate(id,datos,{new:true,runValidators:true});},
     async eliminar(id){return Juego.frindByIdAndDelete(id)},
 };
