515054b900291f03595ab5616b0ead989498b17e
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 12:47:21 -0600

    fix correxion  de errores en validaciones

--- api/src/validators/juego-validator.js
+++ api/src/validators/juego-validator.js
@@ -3,8 +3,10 @@ import {body,param,validationResult} from "express-validator";
 const responderErrores=(req,res,next)=>{
     const errores = validationResult(req);
     if(!errores.isEmpty()){
-        }
-}
+            return res.status(400).json({ errores: errores.array() });
+    }
+    next();
+};
 
 
 
