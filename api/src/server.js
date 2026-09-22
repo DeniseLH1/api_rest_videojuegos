c19b1142a5744b268efbf28c9aa8ff05fe4f7fb2
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 12:23:40 -0600

    fix: correxion de código en el servidor

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,15 @@
-import mongoose from " mongoose";
+import mongoose from "mongoose";
 import app from "./app.js";
 
 const PORT= process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/api-videojuegos";
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/api_juegos_clase";
 
 try{
     await mongoose.connect(MONGO_URI);
+
     console.log("Conectado a Mongo:",MONGO_URI);
+    
 }catch(error){
     console.error("Error al conectar ",error.message);
     process.exit(1);
