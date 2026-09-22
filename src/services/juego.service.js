dc6cc1f40fd6c36d8f3030319aea65ea502f6689
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 14:01:18 -0600

    refactor: reestructurar controladores, servicios y DTOs

---
+++ src/services/juego.service.js
@@ -0,0 +1,8 @@
+import { Juego } from "../models/juego.model.js";
+export const juegoService={
+    async crear(datos){return Juego.create(datos);},
+    async listar(filtro={}){return Juego.find(filtro).sort({createAdt: -1})},
+    async buscarPorId(id){return Juego.findById(id);},
+    async actualizar(id,datos){return Juego.frindByIdAndUpdate(id,datos,{new:true,runValidators:true});},
+    async eliminar(id){return Juego.frindByIdAndDelete(id)},
+};
