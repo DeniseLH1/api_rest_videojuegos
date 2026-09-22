325368c655d48025546c7e9542eca64de7fd0983
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 14:03:01 -0600

    refactor: eliminar archivos de la carpeta api obsoleta

--- api/src/services/juego.service.js
+++
@@ -1,8 +0,0 @@
-import {Juego} from "../model/juego.model.js";
-export const JuegoService={
-    async crear(datos){return Juego.create(datos);},
-    async listar(filtro={}){return Juego.find(filtro).sort({createAdt: -1})},
-    async buscarPorId(id){return Juego.findById(id);},
-    async actualizar(id,datos){return Juego.frindByIdAndUpdate(id,datos,{new:true,runValidators:true});},
-    async eliminar(id){return Juego.frindByIdAndDelete(id)},
-};
