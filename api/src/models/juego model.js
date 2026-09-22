84d11969cabe14fa3163ceadd34f7c2d453f76c9
DeniseLH1 <jdlhprincesa@gmail.com>
2026-09-22 12:00:57 -0600

    feat: se agrego models

--- api/src/models/juego model.js
+++ api/src/models/juego model.js
@@ -1,5 +1,16 @@
-import {Schema,model} from "mongoose";
+import { Schema, model } from "mongoose";
 
-const juegoSchema =  new Schema({
-    titulo: { type:String, required: true ,trim:true,minLenght:2,maxlenght:60},
-})
+const juegoSchema = new Schema({
+    titulo: { type: Tring, requiered: true, trim: true, minLength: 2, maxLength: 60 },
+
+    genero: { type: string, required: true, enum: [ "accion", "rpg", "aventura", "estrategia", "plataformas" ] },
+
+    plataforma: { type: String, required: true, enum: ["pc", "playstation", "xbox", "nintendo"] },
+
+    horasJugadas: { type: Number, default: 0, min: 0 },
+
+    completado: { type: Boolean, default: false },
+
+}, { timestamps: true, versionKey: false  });
+
+export const Juego = model("Juego", juegoSchema);
